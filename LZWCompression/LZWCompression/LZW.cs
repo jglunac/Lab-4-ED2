29a043619c6984f7d123ee16af0497cb48d64b5b
BrazilBatres <60472938+BrazilBatres@users.noreply.github.com>
2020-10-19 20:20:01 -0600

    Corrección Compresión

--- LZWCompression/LZWCompression/LZW.cs
+++ LZWCompression/LZWCompression/LZW.cs
@@ -10,8 +10,11 @@ namespace LZWCompression
     {
         #region Properties
         string Path;
-        Dictionary<string, Character> Characters = new Dictionary<string, Character>();
+        //El diccionario puede ser <string, int>?
+        Dictionary<string, int> Characters = new Dictionary<string, int>();
         List<byte> FinalBytes = new List<byte>();
+        int IDBits = 0;
+        Queue<int> IDqueue;
         #endregion
 
         #region Constructor
@@ -24,10 +27,35 @@ namespace LZWCompression
         #region Compress
         public byte[] Compress()
         {
+            IDqueue = new Queue<int>();
             GetOriginalCharacters();
             GenerateMeta();
             GetStrings();
+            
+            AddIDQueue();
             return FinalBytes.ToArray();
+        }
+        void AddIDQueue()
+        {
+            int count = IDqueue.Count;
+            string BinaryByte = "";
+            string binary = "";
+            for (int i = 0; i < count; i++)
+            {
+                binary = (Convert.ToString(IDqueue.Dequeue(), 2)).PadLeft(IDBits, '0');
+                int missingBits = 8 - BinaryByte.Length;
+                if (missingBits <= binary.Length)
+                {
+                    BinaryByte += binary.Substring(0, missingBits);
+                    binary = binary.Remove(0, missingBits);
+                }
+                else BinaryByte += binary;
+                if (BinaryByte.Length == 8)
+                {
+                    FinalBytes.Add(Convert.ToByte(BinaryByte, 2));
+                    BinaryByte = binary;
+                }
+            }
         }
 
         void GetOriginalCharacters()
@@ -38,17 +66,17 @@ namespace LZWCompression
                 {
                     int counter = 0;
                     string Cadena;
-                    StringBuilder sb = new StringBuilder();
+                    
                     int i = 1;
                     while (counter < fs.Length)
                     {
                         Cadena = Convert.ToChar(reader.ReadByte()).ToString();
                         if (!Characters.ContainsKey(Cadena))
                         {
-                            Character character = new Character();
-                            character.Value = Cadena;
-                            character.Key = i;
-                            Characters.Add(Cadena, character);
+                            //Character character = new Character();
+                            //character.Value = Cadena;
+                            //character.Key = i;
+                            Characters.Add(Cadena, i);
                             i++;
                         }
                         
@@ -65,67 +93,85 @@ namespace LZWCompression
                 using (BinaryReader reader = new BinaryReader(fs))
                 {
                     int counter = 0;
-                    string Cadena = Convert.ToChar(reader.ReadByte()).ToString();
-                    StringBuilder sb = new StringBuilder();
-                    StringBuilder resultbuilder = new StringBuilder();
+                    //string CurrentString = Convert.ToChar(reader.ReadByte()).ToString();
+                    StringBuilder CurrentString = new StringBuilder();
+                    CurrentString.Append(Convert.ToChar(reader.ReadByte()).ToString());
+                    //StringBuilder resultbuilder = new StringBuilder();
                     int i = Characters.Count + 1;
-                    Character AuxCharacter = new Character();
-                    string result;
+                    int ExistentStringID = 0;
+                    //Character AuxCharacter = new Character();
+                    //string result;
                     while (counter < fs.Length)
                     {
-                        sb = new StringBuilder();
-                        if (Characters.ContainsKey(Cadena))
+                        //CurrentString = new StringBuilder();
+                        if (Characters.ContainsKey(CurrentString.ToString()))
                         {
                             counter++;
-                            sb.Append(Cadena);
-                            Characters.TryGetValue(Cadena, out Character character);
-                            AuxCharacter = character;
+                            //CurrentString.Append(CurrentString);
+                            Characters.TryGetValue(CurrentString.ToString(), out ExistentStringID);
+                            
+                            //AuxCharacter = character;
                             if (counter<fs.Length)
                             {
-                                sb.Append(Convert.ToChar(reader.ReadByte()).ToString());
+                                CurrentString.Append(Convert.ToChar(reader.ReadByte()).ToString());
                             }
                             else
                             {
-                                string Binary = Convert.ToString(AuxCharacter.Key, 2);
-                                Binary.PadLeft(8, '0');
-                                byte TargetByte = Convert.ToByte(Binary, 2);
-                                FinalBytes.Add(TargetByte);
+                                string Binary = Convert.ToString(ExistentStringID, 2);
+                                //Binary.PadLeft(8, '0');
+                                //byte TargetByte = Convert.ToByte(Binary, 2);
+
+                                if (Binary.Length > IDBits)
+                                {
+                                    IDBits = Binary.Length;
+                                }
+                                IDqueue.Enqueue(ExistentStringID);
+
                             }
-                            Cadena = sb.ToString();
-                            
+                            //CurrentString = CurrentString.ToString();
+
                         }
                         else
                         {
-                            string Binary = Convert.ToString(AuxCharacter.Key, 2);
-                            byte TargetByte = Convert.ToByte(Binary,2);
-                            FinalBytes.Add(TargetByte);
+                            string Binary = Convert.ToString(ExistentStringID, 2);
+                            //byte TargetByte = Convert.ToByte(Binary,2);
+                            //FinalBytes.Add(TargetByte);
+                            if (Binary.Length > IDBits)
+                            {
+                                IDBits = Binary.Length;
+                            }
+                            IDqueue.Enqueue(ExistentStringID);
                             //resultbuilder.Append(Convert.ToString(AuxCharacter.Key, 2));
-                            Character character = new Character();
-                            character.Value = Cadena;
-                            character.Key = i;
-                            Characters.Add(Cadena, character);
-                            char LastChar = Convert.ToChar(Cadena.Substring(Cadena.Length - 1, 1));
-                            Cadena = LastChar.ToString();
+                            //Character character = new Character();
+                            //character.Value = CurrentString;
+                            //character.Key = i;
+                            Characters.Add(CurrentString.ToString(), i);
+                            string LastChar = CurrentString.ToString(CurrentString.Length - 1, 1);
+                            CurrentString.Clear();
+                            CurrentString.Append(LastChar);
                             i++;
                         }
                         
                     }
-                    result = resultbuilder.ToString();
+                    FinalBytes.Insert(0, Convert.ToByte(IDBits));
+                    //result = resultbuilder.ToString();
                 }
             }
         }
 
         void GenerateMeta()
         {
-            string DiffertentChars = Convert.ToString(Characters.Count, 2);
-            DiffertentChars.PadLeft(8, '0');
-            byte toMeta = Convert.ToByte(DiffertentChars, 2);
-            FinalBytes.Add(toMeta);
+            //string DifferentChars = Convert.ToString(Characters.Count, 2);
+            //DifferentChars.PadLeft(8, '0');
+            //byte toMeta = Convert.ToByte(DifferentChars, 2);
+            //FinalBytes.Add(toMeta);
+           
+            FinalBytes.Add(Convert.ToByte(Characters.Count));
             foreach (var item in Characters)
             {
-                char baseChar = Convert.ToChar(item.Value.Value);
-                byte baseByte = Convert.ToByte(baseChar);
-                FinalBytes.Add(baseByte);
+                //char baseChar = Convert.ToChar(item.Value.Value);
+                //byte baseByte = Convert.ToByte(baseChar);
+                FinalBytes.Add((byte)Convert.ToChar(item.Key));
             }
         }
         #endregion
